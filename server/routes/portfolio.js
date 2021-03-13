92328cff460270fe461131bce3666c5a351db3d6
avery <averywald.dev@gmail.com>
2021-03-13 14:03:42 -0600

    API can query blurbs based on sectionID

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -11,6 +11,7 @@ const options = {
   root: path.join(`${__dirname}/../public/data/`)
 };
 
+// TODO : shoud this return all blurbs related to a section ID?
 router.get('/sections/:id', (req, res, next) => {
   var sectionId = req.params.id;
 
@@ -21,6 +22,30 @@ router.get('/sections/:id', (req, res, next) => {
     const coll = db.collection('sections');
 
     coll.find({ id: Number(sectionId) }).toArray((err, docs) => {
+
+      res.send(docs, options, (err) => {
+        if (err) next(err);
+        else console.log(`sent docs`);
+      });
+
+    });
+
+  });
+
+});
+
+// TODO : blurbs endpoint should query based on non-section related properties
+router.get('/blurbs/:sectionId', (req, res, next) => {
+
+  var sectionId = req.params.sectionId;
+
+  client.connect((err) => {
+    assert.strictEqual(null, err);
+
+    const db = client.db('portfolio');
+    const coll = db.collection('blurbs');
+
+    coll.find({ sectionId: Number(sectionId) }).toArray((err, docs) => {
 
       res.send(docs, options, (err) => {
         if (err) next(err);
