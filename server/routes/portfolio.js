ee147cd63bd210e0ac152cbbf7adfe36b251dd99
avery <averywald.dev@gmail.com>
2021-03-12 23:15:50 -0600

    server returns json data

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var options = {
+    root: path.join(`${__dirname}/../public/data/`)
+  }
+
+  var fileName = 'testSections.json';
+
+  res.sendFile(fileName, options, (err) => {
+    if (err) next(err);
+    else console.log(`sent ${fileName}`);
+  });
 });
 
 module.exports = router;
