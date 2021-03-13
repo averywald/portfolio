4c395c278556563892de8f969184c4eb1ddc74fb
avery <averywald.dev@gmail.com>
2021-03-13 13:29:22 -0600

    set up mongodb, server can get data docs

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var path = require('path');
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
 var router = express.Router();
+
+const url = 'mongodb://localhost:27017';
+const client = new mongo(url);
 
 const options = {
   root: path.join(`${__dirname}/../public/data/`)
@@ -10,6 +15,20 @@ router.get('/', (req, res, next) => {
   var section = (req.query.section) ? req.query.section : '*';
 
   var fileName = 'testSections.json';
+
+  client.connect((err) => {
+    assert.equal(null, err);
+    console.log('successfully connected to db');
+
+    const db = client.db('portfolio');
+    const coll = db.collection('sections');
+    
+    coll.find({}).toArray((err, docs) => {
+      console.log(docs);
+    });
+  });
+
+  
 
   res.sendFile(fileName, options, (err) => {
     if (err) next(err);
